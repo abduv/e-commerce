e1c92a5414859be79244eca44e80acc4f150a03f
Abdurrahman Syzdyk <syzdykovabdurrahman@gmail.com>
2022-02-23 16:27:17 +0600

    created env variable for firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBF4EiMbyltL7eSZDVde32Zxr_GOWUBe28",
-    authDomain: "e-commerce-68753.firebaseapp.com",
-    databaseURL: "https://e-commerce-68753-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "e-commerce-68753",
-    storageBucket: "e-commerce-68753.appspot.com",
-    messagingSenderId: "495331646480",
-    appId: "1:495331646480:web:df59911b68677b017fbe95"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 
 export class Firebase {
